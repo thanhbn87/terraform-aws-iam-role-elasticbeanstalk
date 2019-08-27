ac531721649f071ef1a0e7e77cf3435e99f8980b
thanhbn <thanhbn87@gmail.com>
2019-08-27 11:49:35 +0700

    fix output

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "iam_instance_profile" {
   description = "The name of created iam_instance_profile"
-  value       = "${element(concat(aws_iam_instance_profile.ec2.*.name,list("")),0)}"
+  value       = "${element(compact(concat(aws_iam_instance_profile.ec2.*.name,list(""))),0)}"
 }
 
 output "service_name" {
   description = "The service role name of created service_name"
-  value       = "${element(concat(aws_iam_role.service.*.name,list("")),0)}"
+  value       = "${element(compact(concat(aws_iam_role.service.*.name,list(""))),0)}"
 }
