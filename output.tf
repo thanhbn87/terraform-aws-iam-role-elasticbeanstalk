5a745230579db03f1cdb3326edd4fd6ae942125f
thanhbn <thanhbn87@gmail.com>
2019-08-27 12:54:31 +0700

    fix output when input vars not empty

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "iam_instance_profile" {
   description = "The name of created iam_instance_profile"
-  value       = "${element(compact(concat(aws_iam_instance_profile.ec2.*.name,list(""))),0)}"
+  value       = "${var.iam_instance_profile == "" ? element(concat(aws_iam_instance_profile.ec2.*.name,list("")),0) : "" }"
 }
 
 output "service_name" {
   description = "The service role name of created service_name"
-  value       = "${element(compact(concat(aws_iam_role.service.*.name,list(""))),0)}"
+  value       = "${var.service_name == "" ? element(concat(aws_iam_role.service.*.name,list("")),0) : "" }"
 }
